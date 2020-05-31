98d07dac2f762d5f924c715d5512fca9e68d8282
skatpgusskat <skatpgusskat@naver.com>
2020-05-31 17:59:23 +0900

    Mp4, Simple depth, No alpha

---
+++ Assets/Scripts/DepthRecorder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class DepthRecorder : MonoBehaviour
+{
+    public Camera camera;
+
+    public Transform[] targets = new Transform[0];
+    public bool isRecordOn;
+
+    private int frameCount;
+    private StreamWriter streamWriter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EditorApplication.playModeStateChanged += Save;
+
+        if (isRecordOn)
+        {
+            var path = "Assets/Depths/depth.txt";
+            streamWriter = File.CreateText(path);
+        }
+    }
+
+    private void Save(PlayModeStateChange change)
+    {
+        if (change == PlayModeStateChange.ExitingPlayMode)
+        {
+            streamWriter.Close();
+            isRecordOn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isRecordOn)
+        {
+            return;
+        }
+        var zOrderedIndexes = targets
+            .Select((target, index) => (
+                target,
+                distance: Vector3.Distance(camera.transform.position, target.position),
+                index
+            ))
+            .OrderBy(tuple => tuple.distance)
+            .Select(tuple => tuple.index);
+
+        // frame / index / index / index / index \n
+        var text = $"{frameCount},{string.Join(",", zOrderedIndexes)}";
+        streamWriter.WriteLine(text);
+
+        frameCount++;
+    }
+
+    void Destroy()
+    {
+        Debug.Log("bye");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.UI;
@@ -12,42 +13,79 @@ public class Player : MonoBehaviour
     public VideoPlayer VideoPlayerB;
     public VideoPlayer VideoPlayerBackground;
     public ComputeShader mergeShader;
-    private RenderTexture renderTextureResult;
+    public TextAsset depthFile;
+
+    private readonly Dictionary<int, int[]> _frameDepths = new Dictionary<int, int[]>();
+    private RenderTexture _renderTextureResult;
 
     // Start is called before the first frame update
     void Start()
     {
-        renderTextureResult = new RenderTexture(604, 538, 1);
-        renderTextureResult.enableRandomWrite = true;
-        renderTextureResult.Create();
+        _renderTextureResult = new RenderTexture(700, 538, 1);
+        _renderTextureResult.enableRandomWrite = true;
+        _renderTextureResult.Create();
 
-        rawImage.texture = renderTextureResult;
+        rawImage.texture = _renderTextureResult;
+        InitFrameDepths();
+    }
+
+    private void InitFrameDepths()
+    {
+        var lines = depthFile.text
+            .Replace("\r\n", "\n")
+            .Split('\n');
+
+        foreach (var line in lines)
+        {
+            try
+            {
+                var chunks = line.Split(',');
+                var frame = int.Parse(chunks[0]);
+                var depths = chunks.Skip(1).Select(int.Parse).ToArray();
+
+                _frameDepths[frame] = depths;
+            }
+            catch
+            {
+                // ignore
+            }
+        }
+    }
+
+    private int[] GetOrders(int frame)
+    {
+        return _frameDepths[frame]
+            .Select((depth, index) => (depth, index))
+            .OrderBy(tuple => tuple.depth)
+            .Select(tuple => tuple.index)
+            .ToArray();
     }
 
     // Update is called once per frame
-
     private void Merge()
     {
-        rawImage.texture = null;
-        for (var i = 0; i < 1000; i += 1)
+        var frame = VideoPlayerA.frame;
+        if (frame < 0)
         {
-            var kernel = mergeShader.FindKernel("Merge");
-            mergeShader.SetTexture(kernel, "TextureA", VideoPlayerA.texture);
-            mergeShader.SetTexture(kernel, "TextureB", VideoPlayerB.texture);
-            mergeShader.SetTexture(kernel, "TextureBackground", VideoPlayerBackground.texture);
-
-
-            mergeShader.SetTexture(kernel, "resultTexture", renderTextureResult);
-
-            mergeShader.Dispatch(kernel, 1920 / 8, 1080 / 8, 1);
+            return;
         }
-        rawImage.texture = renderTextureResult;
+
+        var orders = GetOrders((int)frame);
+
+        var kernel = mergeShader.FindKernel("Merge");
+        mergeShader.SetTexture(kernel, "TextureA", VideoPlayerA.texture);
+        mergeShader.SetTexture(kernel, "TextureB", VideoPlayerB.texture);
+        mergeShader.SetInt("orderA", orders[0]);
+        mergeShader.SetInt("orderB", orders[1]);
+
+        mergeShader.SetTexture(kernel, "resultTexture", _renderTextureResult);
+
+        mergeShader.Dispatch(kernel, 700 / 8, 538 / 8, 1);
     }
     void Update()
     {
         if (VideoPlayerA.isPrepared
-            && VideoPlayerB.isPrepared
-            && VideoPlayerBackground.isPrepared)
+            && VideoPlayerB.isPrepared)
         {
             Merge();
         }
